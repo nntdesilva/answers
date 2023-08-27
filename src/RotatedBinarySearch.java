79aa2258359350f00dfd2db0a0ddd9c3a4d41c59
Navoda N <nilakshinavoda97@gmail.com>
2023-08-27 15:48:43 +0530

    add answer rbs

---
+++ src/RotatedBinarySearch.java
@@ -0,0 +1,66 @@
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 3};
+        System.out.println(search(arr, 3));
+
+    }
+
+    static int search(int[] arr, int target) {
+        int pivot = findPivot(arr);
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
+        int idx = binarySearch(arr, target, 0, pivot);
+        if (idx != -1) {
+            return idx;
+        }
+        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+
+    }
+
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (isAsc) {
+                if (arr[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (arr[mid] < target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}

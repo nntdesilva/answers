7cce54988376b34e857d1b575a3283df32c774b8
Navoda N <nilakshinavoda97@gmail.com>
2023-08-30 16:40:15 +0530

    add answers

---
+++ src/BSIn2DArrays.java
@@ -0,0 +1,22 @@
+public class BSIn2DArrays {
+
+    public int[] binarySearch(int[][] arr, int target) {
+        int r = 0;
+        int c = arr[0].length - 1;
+
+        while (r < arr.length && c >= 0) {
+            if (arr[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (arr[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+
+
+        }
+        return new int[]{-1, -1};
+
+    }
+}

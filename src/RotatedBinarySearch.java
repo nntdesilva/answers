d46d7919b01319327d507e3a82cc888a4bbeb94c
Navoda N <nilakshinavoda97@gmail.com>
2023-08-30 10:52:23 +0530

    add rbs for an array with duplicate elements

--- src/RotatedBinarySearch.java
+++ src/RotatedBinarySearch.java
@@ -63,4 +63,34 @@ public class RotatedBinarySearch {
         }
         return -1;
     }
+
+    // rotated binary search with duplicate elements
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            } else if (arr[start] <= arr[mid] && arr[mid] > arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
